987eeb07cde608bca2468691adfd1e5c3277a456
Sergio Ramos <sergioramos_92@hotmail.com>
2024-06-06 17:16:53 -0300

    se agrega un comentario en index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import pokemonRoutes from './routes/pokemon.routes'
 
 const app = express()
-const PORT = 3000
+const PORT = 3000 // <-- cambiar si lo desea
 
 app.use(pokemonRoutes)
 
