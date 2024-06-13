3158a7eb208708b67ef1893ecebaa88c313f0093
Sergio Ramos <sergioramos_92@hotmail.com>
2024-06-13 18:45:42 -0300

    se modifica el parámetro limit

--- src/services/pokemon.service.ts
+++ src/services/pokemon.service.ts
@@ -21,7 +21,7 @@ const getByName = async (name: string): Promise<Pokemon | null> => {
 
 const getAll = async (): Promise<PokemonListWrapper> => {
   try {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20')
     const data = await response.json()
     const pokemonListWrapper: PokemonListWrapper = {
       previous: data.previous,
