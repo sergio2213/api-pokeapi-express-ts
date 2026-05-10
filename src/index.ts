7246bfdbd5bb55bf53f5b7659f92f6e5fde844a6
Sergio Ramos <sergioramos_92@hotmail.com>
2026-05-10 19:16:54 -0300

    se mejora la configuración de CORS y se agrega manejo de errores

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,31 @@
 import express from "express";
+import dotenv from "dotenv";
 import cors from "cors";
+import helmet from "helmet";
 import pokemonRoutes from "./routes/pokemon.routes";
+import { errorHandler } from "./middlewares/error-handler.middleware";
 
+dotenv.config();
+const ALLOWED_ORIGINS = process.env.ALLOWED_ORIGINS?.split(", ") || [
+  "http://localhost:5173",
+  "http://localhost:4173",
+];
 const app = express();
-const PORT = 3000; // <-- cambiar si lo desea
+const PORT = process.env.PORT || 3000;
 
-const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:4173"],
+const corsOptions: cors.CorsOptions = {
+  origin: ALLOWED_ORIGINS,
+  methods: ["GET"],
   optionsSuccessStatus: 200,
 };
 
+app.use(helmet());
 app.use(cors(corsOptions));
-app.use(pokemonRoutes);
+app.use("/api/v1", pokemonRoutes);
+
+app.use(errorHandler);
 
 app.listen(PORT, function () {
-  console.log(`Listening at port: ${PORT}`);
+  console.log(`Server running on port: ${PORT}`);
+  console.log("API Version: v1");
 });
