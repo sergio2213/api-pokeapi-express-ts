fe28f9a4b04168783410ebe4900e5fe6229390b3
Sergio Ramos <sergioramos_92@hotmail.com>
2026-05-10 19:21:24 -0300

    se eliminan rutas obsoletas y se actualizan los handlers de Pokemon

--- src/routes/pokemon.routes.ts
+++ src/routes/pokemon.routes.ts
@@ -1,12 +1,9 @@
-import { RequestHandler, Router } from "express";
+import { Router } from "express";
 import pokemonController from "../controllers/pokemon.controller";
 
 const router = Router();
 
-router.get(
-  "/pokemon/:name",
-  pokemonController.getPokemonByName as RequestHandler,
-);
-router.get("/pokemon", pokemonController.listPokemon as RequestHandler);
+router.get("/pokemon", pokemonController.index);
+router.get("/pokemon/:identifier", pokemonController.getOne);
 
 export default router;
